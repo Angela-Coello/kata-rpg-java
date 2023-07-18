a059386738e69328cafcfd69c1c8144a43fef9c1
Angela-coello <angeladarko32@gmail.com>
2023-07-18 10:01:18 +0200

    first commit

--- src/main/java/org/example/character/Character.java
+++ src/main/java/org/example/character/Character.java
@@ -3,7 +3,31 @@ package org.example.character;
 public class Character {
 private int health = 1000;
 
-    public int getHealth() {
-return health;
+    public int getLevel() {
+        return level;
     }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    private int level = 1;
+
+    public int getHealth() {
+       return health;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    private boolean isAlive = true;
+
 }
--- src/test/java/org/example/character/CharacterTest.java
+++ src/test/java/org/example/character/CharacterTest.java
@@ -5,9 +5,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CharacterTest {
+
+
+    Character character = new Character();
 @Test
     public void health_should_start_at_1000(){
-    Character character = new Character();
     assertEquals(1000,character.getHealth());
 }
+
+@Test
+    public void level_should_satart_at_1(){
+    assertEquals(1, character.getLevel() );
 }
+
+@Test
+    public void May_be_Alive_or_Dead_starting_Alive(){
+    assertTrue( character.isAlive());
+}
+}
+
